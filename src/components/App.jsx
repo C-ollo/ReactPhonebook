2a7556346718b89eeea607dfb7ff7a05ef5c3cc3
C_ollo <103580135+C-ollo@users.noreply.github.com>
2023-07-03 08:56:17 +0000

    Update 5 files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,31 @@
 import React from "react";
-import Card from "./Card.jsx";
-import Contacts from "../contacts";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
 function App() {
   return (
     <div>
-      {Contacts.map((contact, index) => (
-        <Card
-          key={index}
-          name={contact.name}
-          img={contact.imgURL}
-          tel={contact.phone}
-          email={contact.email}
-        />
-      ))}
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://pbs.twimg.com/profile_images/744849215675838464/IH0FNIXk.jpg" />
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,17 @@
 import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./Detail";
 
-function Card(Props) {
+function Card(props) {
   return (
-    <div>
-      <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2 className="name">{Props.name}</h2>
-          <img className="circle-img " src={Props.img} />
-        </div>
-        <div className="bottom">
-          <p className="info">{Props.tel}</p>
-          <p className="info">{Props.email}</p>
-        </div>
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <Avatar img={props.img} />
+      </div>
+      <div className="bottom">
+        <Detail detailInfo={props.tel} />
+        <Detail detailInfo={props.email} />
       </div>
     </div>
   );
